2e2c42b5a9588a9eb4c08ca335559b961eb890d6
loku <danielpawlow@gmail.com>
2018-03-10 12:55:54 +0100

    Add project files.

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,62 @@
+﻿using DutchTreat.Data;
+using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService _mailService;
+        private readonly IDutchRepository _repository;
+
+        public AppController(IMailService mailService, IDutchRepository repository)
+        {
+            _mailService = mailService;
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMessage("danielpawlow@gmail.com", model.Subject, $"From: {model.Email} {model.Name} {model.Message}");
+                ViewBag.MailSent = "Mail Sent";
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            var results = _repository.GetAllProducts();
+
+            return View(results);
+        }
+    }
+}
